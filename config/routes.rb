cb77a6f13e82dffe0e1899497eba36f2af25e9d0
Matheus Oliveira <matheusoliveira@cjr.org.br>
2018-08-30 00:20:21 -0300

    adicionando páginas editaveis do devise, realizando root em 'sign_in'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root :to => redirect("/admins/sign_in")
   devise_for :admins
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
