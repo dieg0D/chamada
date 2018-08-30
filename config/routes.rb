8b1a6b5fe93f40e3389a0b902f9de0231da56c10
Matheus Oliveira <matheusoliveira@cjr.org.br>
2018-08-30 10:59:41 -0300

    criando index temporária e habilitando login/logout de admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root :to => redirect("/admins/sign_in")
+  root "list#index"
+  #root :to => redirect("/admins/sign_in")
   devise_for :admins
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
