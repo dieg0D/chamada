c0891d98a23f1f309d6e39577759d706b7926b0b
Matheus Oliveira <matheusoliveira@cjr.org.br>
2018-09-09 15:19:27 -0300

    adicionando validates

--- app/models/attendance_list.rb
+++ app/models/attendance_list.rb
@@ -2,4 +2,7 @@ class AttendanceList < ApplicationRecord
     belongs_to :type
     has_many :status, dependent: :destroy 
  	has_many :user, through: :status
+
+ 	validates :data, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
 	has_many :status, dependent: :destroy 
  	has_many :attendance_list, through: :status
+
+ 	validates :nome, presence: true
+ 	validates :email, presence: true, uniqueness: true
 end
