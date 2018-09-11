ce09f5fd0b4ff2c24f32834784211eb85a40c4a9
Matheus Oliveira <matheusoliveira@cjr.org.br>
2018-09-10 23:49:03 -0300

    removendo lógica de admins individuais das páginas em views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
 	def is_admin
 		if !current_admin
-			if request.env['PATH_INFO'] != "/admins/sign_in"
+			if request.env['PATH_INFO'] != "/admins/sign_in" && request.env['PATH_INFO'] != "/admins/sign_up" 
 				redirect_to new_admin_session_path
 			end
 		end
