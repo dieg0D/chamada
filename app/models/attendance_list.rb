fff2adebb2202344dd24fc36e4d6683c3d173ba2
Gabriel Martins <gsmartins96@hotmail.com>
2018-09-16 12:50:38 -0300

    Front quase 100% e alguns bugs e redirecionamentos corrigidos

--- app/models/attendance_list.rb
+++ app/models/attendance_list.rb
@@ -1,9 +1,7 @@
 class AttendanceList < ApplicationRecord
     belongs_to :type
     has_many :status, dependent: :destroy 
- 	has_many :user, through: :status
-
- 	
- 	validates :data, presence: true
-
+    has_many :user, through: :status
+     
+    validates :data, presence: true
 end
