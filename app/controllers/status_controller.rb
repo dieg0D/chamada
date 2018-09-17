3832c013ee9234471cd36f0637793621d7088e00
dieg0d <diegodi05@hotmail.com>
2018-09-17 20:39:01 -0300

    odenando lista por ordem alfabetica

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -6,7 +6,7 @@ class StatusController < ApplicationController
 	
 	def new
 		@att = AttendanceList.all.last
-		@user = User.all
+		@user = User.all.order(:nome)
 		@status = []
 		@user.each do |u|
 			@status << Status.new(user_id: u.id, attendance_list_id: @att.id, state: "Presente")
@@ -30,7 +30,7 @@ class StatusController < ApplicationController
 
   	def edit
   		@att = AttendanceList.find(params[:id])
-  		@user = User.all
+  		@user = User.all.order(:nome)
   		@status = @att.status
   		puts @status
   	end
