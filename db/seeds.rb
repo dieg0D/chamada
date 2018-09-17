65381e1e8a7ef6aaa195d1e1592966337144b7fd
dieg0d <diegodi05@hotmail.com>
2018-09-17 07:34:00 -0300

    pronto pro deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,27 +10,9 @@
 #categories
 puts "Seeding..."
 
-Admin.create(email: "admin@email.com", password: "admin123", password_confirmation:"admin123");
+Admin.create(email: "admin@admin.com", password: "12345678", password_confirmation:"12345678");
 
 Type.create(name: "Processo Trainee")
 Type.create(name: "REGER")
-
-User.create(nome: "Jorge", email: "jorge@email.com", membro: "true")
-User.create(nome: "Paulo", email: "paulo@email.com", membro: "true")
-User.create(nome: "maria", email: "maria@email.com", membro: "false")
-
-
-AttendanceList.create(data:"10/10/10", description: "REGER 1", type_id: "2");
-AttendanceList.find(1).status.create(user_id: "1", state: "Presente");
-AttendanceList.find(1).status.create(user_id: "2", state: "Presente");
-
-
-AttendanceList.create(data:"10/10/10", description: "REGER 2", type_id: "2");
-AttendanceList.find(2).status.create(user_id: "1", state: "Ausente");
-AttendanceList.find(2).status.create(user_id: "2", state: "Falta Justificada");
-
-
-AttendanceList.create(data:"10/10/10", description: "Primeira aula do PT", type_id: "1");
-AttendanceList.find(3).status.create(user_id: "3", state: "Falta");
 
 puts "Seeding... OK"
