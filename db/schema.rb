b0af047bbf8ddc5adaee6c315eedb5f18efb459e
Gustavo Luigi <gustavolusilo@gmail.com>
2019-01-16 07:50:09 -0200

    Added string column "nucleo" in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_013127) do
+ActiveRecord::Schema.define(version: 2019_01_16_094746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2018_09_04_013127) do
     t.boolean "membro"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nucleo"
   end
 
   add_foreign_key "attendance_lists", "types"
