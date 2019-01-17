1727e89ccb6c3191780b9c1c064ecaa9d56b78c2
Gustavo Luigi <gustavolusilo@gmail.com>
2019-01-17 20:34:53 -0200

    Created '/admins' page. Only index.

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
   get '/reger' => 'users#reger'
   get '/pt' => 'users#trainees'
   
-
-  get "/admins_list" => "list#lista"
+  devise_scope :admin do get "/admins" => "admins/registrations#index" end
   devise_for :admins, controllers: {:registrations => "admins/registrations"}
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
