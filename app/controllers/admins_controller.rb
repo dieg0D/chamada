96aff360b7344cf591a0b4692b4f89264c84ca0b
Gustavo Luigi <gustavolusilo@gmail.com>
2019-01-19 17:36:24 -0200

    Added way to edit admins from "/admins/index" page.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -22,16 +22,36 @@ class AdminsController < ApplicationController
   end
 
   def edit
-  
+    @admin = Admin.find(params[:format])
   end
 
   def update
+    @admin = Admin.find(params[:id])
 
+    if params[:admin][:password].present?
+      params = admin_params
+    else
+      params = admin_params_without_password
+    end
+
+    respond_to do |format|
+      if @admin.update(params)
+        format.html { redirect_to admins_path}
+        format.json { render :show, status: :ok, location: @admin }
+      else
+        format.html { render :edit }
+        format.json { render json: @admin.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
-    def admin_params
-      params.require(:admin).permit(:email, :password, :password_confirmation)
-    end 
+  def admin_params
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+  
+  def admin_params_without_password
+    params.require(:admin).permit(:email)
+  end
 end
