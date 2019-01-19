4e28e3bca9c6140a7a8d78b0739d1e6725427957
Gustavo Luigi <gustavolusilo@gmail.com>
2019-01-19 17:12:01 -0200

    Added proper way to add a new admin from '/admins/index' page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,11 +4,21 @@ class AdminsController < ApplicationController
   end
 
   def new
-
+    @admin = Admin.new
   end
 
   def create
-    
+    @admin = Admin.new(admin_params)
+
+    respond_to do |format|
+      if @admin.save
+        format.html { redirect_to admins_path}
+        format.json { render :show, status: :created, location: @admin }
+      else
+        format.html { render :new }
+        format.json { render json: @admin.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -18,4 +28,10 @@ class AdminsController < ApplicationController
   def update
 
   end
+
+  private
+
+    def admin_params
+      params.require(:admin).permit(:email, :password, :password_confirmation)
+    end 
 end
