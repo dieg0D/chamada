d84162f64b37a5a4346183cd07e13dd88bb0dbab
Gustavo Luigi <gustavolusilo@gmail.com>
2019-02-21 17:19:23 -0300

    Send an email every time there is a new attedance list and tells who is missing

---
+++ test/mailers/attendance_list_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AttendanceListMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/attendance_list_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/attendance_list_mailer
+class AttendanceListMailerPreview < ActionMailer::Preview
+
+end
